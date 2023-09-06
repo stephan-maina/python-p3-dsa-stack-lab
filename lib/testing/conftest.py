bbd168a0b9f8a85961f01833afcc322dae300590
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-06 12:38:58 +0300

    Completed

--- lib/testing/conftest.py
+++ lib/testing/conftest.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+import pytest
+from Stack import Stack
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+@pytest.fixture
+def empty_stack():
+    return Stack()
+
+@pytest.fixture
+def sample_stack():
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    return s
